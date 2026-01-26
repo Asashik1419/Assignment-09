a41186b37d8fc87809dd400a2d3a49c0aeff96b2
As Ashik <your-email@example.com>
2026-01-27 00:47:30 +0600

    added registerPage function

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHsVbH8OAJUyF8pLwGht1DajnbMZvRfuY",
+  authDomain: "assignment-9-a378b.firebaseapp.com",
+  projectId: "assignment-9-a378b",
+  storageBucket: "assignment-9-a378b.firebasestorage.app",
+  messagingSenderId: "423198197140",
+  appId: "1:423198197140:web:926d6f39306738b7937b81"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/page/RegisterPage/RegisterPage.jsx
+++ src/page/RegisterPage/RegisterPage.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const RegisterPage = () => {
+    const {creatUser , setUser} = use (AuthContext);
+    const handleRegister=(e)=>{
+        e.preventDefault();
+        console.log(e.target);
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log({name,photo,email,password});
+        creatUser(email,password)
+        
+        .then(result=>{
+            const user=result.user;
+            // console.log(user);
+            setUser(user);
+        })
+        .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(errorMessage);
+    // ..
+  });
+    }
     return (  
   <div className="hero-content mx-auto flex-col lg:flex-row-reverse">
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         
-      <div className="card-body">
+      <form onSubmit={handleRegister} className="card-body">
         <fieldset className="fieldset">
              {/* name */}
           <label className="label">Name</label>
-          <input type="text" className="input" placeholder="Name" />
+          <input name='name' type="text" className="input" placeholder="Name" required />
           {/* photo url */}
           <label className="label">photo URL</label>
-          <input type="text" className="input" placeholder="photo URL" />
+          <input name='photo' type="text" className="input" placeholder="photo URL" required />
             {/* email */}
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input name='email' type="email" className="input" placeholder="Email" required />
           {/* password */}
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <button className="btn btn-neutral mt-4">Register</button>
+          <input name='password' type="password" className="input" placeholder="Password" required />
+          <button type='submit' className="btn btn-neutral mt-4">Register</button>
           <p className='text-center pt-5 text-[16px]'>Allready Have an account <Link to={'/loginPage'}><span className='text-secondary'>Login?</span></Link></p>
         </fieldset>
-      </div>
+      </form>
     </div>
   </div>
 
